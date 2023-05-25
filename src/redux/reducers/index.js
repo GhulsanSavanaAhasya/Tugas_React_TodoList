9c4d87f8579a558edf8c953c4f8db4fadc7a0b3e
Ghulsan Savana Ahasya <ghulsan.savanaa@gmail.com>
2023-05-25 23:00:50 +0700

    mengganti nama variabel dan menambahkan default

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -4,14 +4,16 @@ const initialState = {
     todos: [],
 }
 
-const items = (state = initialState, action) => {
+const indexReducer = (state = initialState, action) => {
     switch(action.type){
         case ADD_ITEM:
             return{
                 ...state,
                 todos: [...state.todos, action.payload],
             }
+        default:
+            return state
     }
 }
 
-export default items
+export default indexReducer

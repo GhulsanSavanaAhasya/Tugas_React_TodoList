f5f12395b7543c7028cab62f8465dd36a247d90e
Ghulsan Savana Ahasya <ghulsan.savanaa@gmail.com>
2023-05-26 10:16:23 +0700

    mengubah add todo dan menambahkan edit dan title

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,15 +1,50 @@
-import { ADD_ITEM } from "../actions/actionTypes"
+import { ADD_ITEM, EDIT_ITEM, SET_ITEM, SET_TITLE } from "../actions/actionTypes"
 
 const initialState = {
     todos: [],
+    title: "",
+    todo: "",
 }
 
 const indexReducer = (state = initialState, action) => {
     switch(action.type){
         case ADD_ITEM:
-            return{
+            const newTodo = {
+                id: Date.now(),
+                value: state.title,
+                isDone: false,
+            }
+        return {
+            ...state,
+            todos: state.todos.concat(newTodo),
+            title: "",
+        }
+        case SET_TITLE:
+            return {
                 ...state,
-                todos: [...state.todos, action.payload],
+                title: action.title,
+            }
+        
+        case SET_ITEM:
+            return {
+                ...state,
+                todo: action.todo,
+            }
+        case EDIT_ITEM:
+            var newList = [...state.todos]
+            var index = newTodo.indexOf(state.todo)
+            if(index !== -1){
+                newList[index].value = state.title
+                return{
+                    ...state,
+                    title: "",
+                    edit: false,
+                    todos: newTodo,
+                }
+            }else {
+                return{
+                    ...state
+                }
             }
         default:
             return state

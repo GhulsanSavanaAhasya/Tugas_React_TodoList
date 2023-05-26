3095fadd35715f7acca955e1e9ab756f19753a65
Ghulsan Savana Ahasya <ghulsan.savanaa@gmail.com>
2023-05-26 16:28:31 +0700

    membuat dan menambahkan edit, delete, dan done

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,21 +1,39 @@
-import { connect } from "react-redux"
-import { addItem, editItem, setTitle } from "../redux/actions";
+import { connect } from "react-redux";
+import {
+  addItem,
+  delItem,
+  editItem,
+  setDone,
+  setEdit,
+  setItem,
+  setTitle,
+} from "../redux/actions";
+import {
+  GrCheckbox,
+  GrCheckboxSelected,
+  GrTrash,
+  GrEdit,
+} from "react-icons/gr";
 
 function TodoList({
   title,
   addItem,
   editItem,
   edit,
-  setTitle,
+  setDone,
   todoList,
+  setTitle,
+  setItem,
+  setEdit,
+  delItem,
 }) {
   const handleChange = (event) => {
     const title = event.target.value;
-    setTitle(title)
+    setTitle(title);
   };
 
   const handleClick = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     if (title.length === 0) {
       return;
     }
@@ -25,9 +43,25 @@ function TodoList({
       addItem();
       console.log(title);
     }
-  }
+  };
 
-  console.log(todoList)
+  const handleDone = (todo) => {
+    setItem(todo);
+    setDone();
+  };
+
+  const handleDel = (todo) => {
+    setItem(todo);
+    delItem();
+  };
+
+  const handleEdit = (todo) => {
+    setTitle(todo.value);
+    setEdit();
+    setItem(todo);
+  };
+
+  // console.log(todoList);
 
   return (
     <>
@@ -47,6 +81,35 @@ function TodoList({
           </form>
         </div>
       </div>
+
+      {todoList &&
+        todoList.map((todo) => {
+          return (
+            <div key={todo.id} className="wrapper-list">
+              <div className="card-list">
+                <div onClick={() => handleDone(todo)}>
+                  {!todo.isDone && <GrCheckbox></GrCheckbox>}
+                  {todo.isDone && <GrCheckboxSelected></GrCheckboxSelected>}
+                </div>
+                <span
+                  style={{
+                    textDecoration: todo.isDone ? "line-through" : "none",
+                  }}
+                >
+                  {todo.value}
+                </span>
+                <div className="btn-list">
+                  <div className="btn-edit" onClick={() => handleEdit(todo)}>
+                    <GrEdit></GrEdit>
+                  </div>
+                  <div className="btn-del" onClick={() => handleDel(todo)}>
+                    <GrTrash></GrTrash>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
     </>
   );
 }
@@ -56,16 +119,19 @@ const mapStateToProps = (state) => {
     edit: state.edit,
     title: state.title,
     todoList: state.todos,
-  }
-}
+  };
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
     addItem: () => dispatch(addItem()),
     editItem: () => dispatch(editItem()),
-    setItem: (item) => dispatch(setItem(item)),
+    setItem: (todo) => dispatch(setItem(todo)),
     setTitle: (title) => dispatch(setTitle(title)),
-  }
-}
+    delItem: (todo) => dispatch(delItem(todo)),
+    setDone: () => dispatch(setDone()),
+    setEdit: () => dispatch(setEdit()),
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(TodoList)
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
